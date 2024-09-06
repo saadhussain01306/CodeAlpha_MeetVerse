372354b60a33ba17f193158327f9e145aa9db12e
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-09-06 20:00:35 +0530

    Update AgoraRTMSetup.js

--- Client/src/AgoraRTMSetup.js
+++ Client/src/AgoraRTMSetup.js
@@ -1,6 +1,6 @@
 import { createClient } from 'agora-rtm-react';
 
-const appId = "8981c03efde9489b89e319203880ba12";
+/* your agora project key*/ const appId = "";
 const token = null;
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useRTMClient = createClient(appId);
