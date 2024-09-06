95745bdec71c201eba92581b886bedce94d8ae9c
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-09-06 20:00:13 +0530

    Update AgoraSetup.js

--- Client/src/AgoraSetup.js
+++ Client/src/AgoraSetup.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAndCameraTracks, createScreenVideoTrack }
 
 import AgoraRTM from 'agora-rtm-sdk';
 
-const appId = "8981c03efde9489b89e319203880ba12";
+/* your agora project key*/ const appId = "";
 const token = null;
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
